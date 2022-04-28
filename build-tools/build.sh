841e3710990a6fe0208fa9aae19af5a9e5d052a3
Ludovic Lamarre <ludo+github@rl82.com>
2022-04-28 19:53:08 -0400

    Update of build.sh to build also tools

--- build-tools/build.sh
+++ build-tools/build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-# Build console
+# Create tmp folder
+mkdir -p tmp/
+
+# Populate vars
 USER="$(whoami)"
-docker build . --iidfile iid --label maintainer=$USER --build-arg USER=$USER
+
+# Build console
+read -p "Build console (y|n)? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  docker build . --iidfile tmp/iid-console --label maintainer=$USER --build-arg USER=$USER -t console -f Dockerfile-console
+fi
+
+# Build tools
+for tool in $(ls Dockerfile-*); do
+  if [ "$tool" != "Dockerfile-console" ]; then
+    t=${tool//Dockerfile-/} 
+    read -p "Build $t (y|n)? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      docker build . --iidfile tmp/iid-$t -t $t -f $tool
+    fi
+  fi
+done
