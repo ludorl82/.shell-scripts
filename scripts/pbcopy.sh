77cbff5a5a6f049a57bb2c64cd32dfeafa785a79
Ludovic Lamarre <ludo+github@rl82.com>
2022-08-11 21:29:23 -0400

    Fixing copy with ssh session

--- scripts/pbcopy.sh
+++ scripts/pbcopy.sh
@@ -2,17 +2,20 @@
 
 if [[ ! -z "$DISPLAY" ]]; then
   xclip </dev/stdin
-elif [[ "$CLIENT" = "termux" ]]; then
-  string="$(</dev/stdin)"
-  echo -n "$string" | ssh -p8022 u0_a311@localhost 'cat | termux-clipboard-set'
 elif [[ "$CLIENT" = "mintty" ]]; then
   string="$(</dev/stdin)"
   if [[ "$(tmux ls | grep mintty-ssh | wc -l)" -eq "0" ]]; then
-    tmux new-session -d -s mintty-ssh 'ssh -o StrictHostKeyChecking=no -p8023 rpsja772@127.0.0.1'
+    tmux new-session -d -s mintty-ssh 'ssh -o StrictHostKeyChecking=no -p8023 ludorl82@127.0.0.1'
     sleep 2
   fi
-  echo -n "$string" > /home/rpsja772/tmp/mintty-ssh
-  tmux send-keys -t mintty-ssh "ssh -o StrictHostKeyChecking=no -p2222 rpsja772@127.0.0.1 cat /home/rpsja772/tmp/mintty-ssh > /dev/clipboard" ENTER
+  echo -n "$string" > /home/ludorl82/tmp/mintty-ssh
+  tmux send-keys -t mintty-ssh "ssh -o StrictHostKeyChecking=no -p2222 ludorl82@127.0.0.1 cat /home/ludorl82/tmp/mintty-ssh > /dev/clipboard" ENTER
 else
-  echo "No clipboard sync method" && exit 0
+  string="$(</dev/stdin)"
+  if [[ "$(tmux ls | grep termux-ssh | wc -l)" -eq "0" ]]; then
+    tmux new-session -d -s termux-ssh 'ssh -o StrictHostKeyChecking=no -p8022 u0_a311@127.0.0.1'
+    sleep 2
+  fi
+  echo -n "$string" > /home/ludorl82/tmp/termux-ssh
+  tmux send-keys -t termux-ssh "ssh -o StrictHostKeyChecking=no -p2222 ludorl82@ludorl82-virtual-machine.local cat /home/ludorl82/tmp/termux-ssh | termux-clipboard-set" ENTER
 fi
