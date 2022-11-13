13a497e9e24a4a3990b2be38d21b84ec131fd23e
Ludovic Lamarre <ludo+github@rl82.com>
2022-11-13 11:44:24 -0500

    Adding terminal clipboard copy

--- scripts/pbcopy.sh
+++ scripts/pbcopy.sh
@@ -10,6 +10,15 @@ elif [[ "$CLIENT" = "mintty" ]]; then
   fi
   echo -n "$string" > /home/ludorl82/tmp/mintty-ssh
   tmux send-keys -t mintty-ssh "ssh -o StrictHostKeyChecking=no ludorl82@localhost cat /home/ludorl82/tmp/mintty-ssh > /dev/clipboard" ENTER
+elif [[ "$CLIENT" = "terminal" ]]; then
+  string="$(</dev/stdin)"
+  if [[ "$(tmux ls | grep terminal-ssh | wc -l)" -eq "0" ]]; then
+    tmux new-session -d -s terminal-ssh 'ssh -o StrictHostKeyChecking=no 172.17.0.1'
+    sleep 2
+    tmux send-keys -t terminal-ssh "export DISPLAY=':10.0'" ENTER
+  fi
+  echo -n "$string" > /home/ludorl82/tmp/terminal-ssh
+  tmux send-keys -t terminal-ssh "ssh -o StrictHostKeyChecking=no -p2222 localhost cat /home/ludorl82/tmp/terminal-ssh | xclip -selection clipboard" ENTER
 else
   string="$(</dev/stdin)"
   if [[ "$(tmux ls | grep termux-ssh | wc -l)" -eq "0" ]]; then
@@ -17,5 +26,5 @@ else
     sleep 2
   fi
   echo -n "$string" > /home/ludorl82/tmp/termux-ssh
-  tmux send-keys -t termux-ssh "ssh -o StrictHostKeyChecking=no -p2222 ludorl82@ludorl82-virtual-machine.local cat /home/ludorl82/tmp/termux-ssh | termux-clipboard-set" ENTER
+  tmux send-keys -t termux-ssh "ssh -o StrictHostKeyChecking=no -p2222 ludorl82@ludorl82-Super-Server.local cat /home/ludorl82/tmp/termux-ssh | termux-clipboard-set" ENTER
 fi
